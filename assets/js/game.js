d6615564332fe45b80fdfb34f6f96d4ab41cdbaa
austindickey <austindickey@me.com>
2020-02-04 19:55:31 -0600

    adding footer and online player number tracker

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,6 +21,20 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig)
 var database = firebase.database()
 
+var connectionsRef = database.ref("/connections")
+var connectedRef = database.ref(".info/connected")
+
+connectedRef.on("value", function(snap) {
+  if (snap.val()) {
+    var con = connectionsRef.push(true)
+    con.onDisconnect().remove()
+  }
+})
+
+connectionsRef.on("value", function(snapshot) {
+    $("#playerCount").text(snapshot.numChildren())
+})
+
 function gameStart() {
     $(".choices").on("click", function () {
 
